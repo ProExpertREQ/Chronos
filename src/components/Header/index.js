e61df825c1d0faa6188ebe1cfb620a9c808e14af
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-21 22:35:48 -0300

    :bulb: Add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Navbar } from './styles';
+
+export default function Header () {
+  return (
+    <Navbar>
+      <h1>Chronos</h1>
+      <button>Começar</button>
+      <button>Entrar</button>
+    </Navbar>
+  );
+};
