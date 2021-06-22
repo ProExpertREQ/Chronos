3ba944631cede9c6a3ab12e6cf09c961fb83c071
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-22 20:40:49 -0300

    :bulb: Add colors

---
+++ src/components/styles/colors.js
@@ -0,0 +1,13 @@
+ const colors = {
+  light: '#89C8FF',
+  main: '#3FB1B5',
+  dark: '#184244',
+
+  yellow: '#FFCF2D',
+  green: '#2AC769',
+  red: '#FE5244',
+
+  white: '#F2F9FA',
+};
+
+export default colors;
