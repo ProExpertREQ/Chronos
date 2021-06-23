a05eb301a3f5ec1a169b7a9b8685a4c97eb28f11
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-23 19:03:40 -0300

    :bulb: Add ReadMore styles

---
+++ src/components/ReadMore/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import colors from '../styles/colors';
+
+export const Container = styled.section`
+  background-color: ${colors.white};
+  position: relative;
+  top: -7.5rem;
+  
+  h1{
+    margin: 0;
+  }
+`;
