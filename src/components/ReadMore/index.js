d7f4e327691748b16f3ccc4a4298f92fc0d15601
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-23 18:58:28 -0300

    :arrow_up: Update - include ReadMore

---
+++ src/components/ReadMore/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function ReadMore () {
+  return (
+    <Container>
+      <h1>Hi, I'm Read More.</h1>
+    </Container>
+  );
+};
