1af92a54d3e547a573f80a45f3c94659fb263d7c
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-26 22:37:51 -0300

    :bulb: Add Feature component

---
+++ src/components/Features/Feature/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Feature ({ description }) {
+  return (
+    <Container>
+      <p>{description}</p>
+    </Container>
+  );
+}
