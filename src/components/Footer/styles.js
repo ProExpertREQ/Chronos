1906f8affc00dddebba9fefd82ed49e845f682a8
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-26 22:38:57 -0300

    :cyclone: Refactoring Footer styles

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -6,56 +6,73 @@ export const Container = styled.footer`
   background-color: ${colors.main};
   display: flex;
   flex-direction: column;
-  align-items: stretch;
+  justify-content: space-between;
+  align-items: center;
+  font-family: 'Baloo Tamma 2', cursive;
   color: ${colors.white};
-  /* height: 20rem; */
-
-  svg {
-    position: absolute;
-    top: 70rem;
-  }
-
-  h2 {
-    margin: 1rem 0 0 0;
-    font-size: 2rem;
-    text-align: center;
-  }
 
   p {
-    margin: 0 27rem 0 27rem;
+    margin: 0;
     text-align: center;
     font-weight: 500;
   }
 
-  button {
+  .about {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1.5rem;
+
+    .about-text {
+      font-size: 1.2rem;
+      margin: -.3rem 0 1rem 0;
+    }
+
+    h2 {
+      margin: 0;
+      font-size: 2rem;
+      text-align: center;
+    }
+
+    button {
       display: flex;
       justify-content: center;
       align-items: center;
-      margin: 1rem 0;
-      width: 20rem;
-      height: 3rem;
-      border-radius: 0.9rem;
+
+      padding: .4rem 3rem;
+      border-radius: .9rem;
       background-color: ${colors.dark};
-      font-family: 'Baloo Tamma 2', cursive;
-      font-size: 1.5rem;
-      font-weight: 500;
-      color: ${colors.white};
       border: none;
       transition: 0.3s;
-      align-self: center;
+
+      p {
+        font-weight: 700;
+        font-size: 1.5rem;
+        color: ${colors.white};
+        font-family: 'Baloo Tamma 2', cursive;
+      }
 
       :hover {
         opacity: 0.95;
         transform: scale(1.05);
+        cursor: pointer;
       }
       :active {
         transition: 0.001s;
         opacity: 0.7;
       }
-
-      img {
-        width: 10%;
-        margin-right: 1rem;
+      ::-moz-selection {
+        color: ${colors.white};
+        background: transparent;
       }
+      ::selection {
+        color: ${colors.white};
+        background: transparent;
+      }
+      img {
+        margin-right: 1rem;
+      } 
+    }
   }
 `;
