fea928b73d3421b1af06b3d1ac51ccca9ce74ce7
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-28 21:24:53 -0300

    :cyclone: Refactoring Home

---
+++ src/logged_out/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './Header';
+import Features from './Features';
+
+export default function Home () {
+  return (
+    <>
+      <Header />
+      <Features />
+    </>
+  );
+}
