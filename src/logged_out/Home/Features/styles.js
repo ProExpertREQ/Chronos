a16497d0d111fa8dcc0da8a53e0996ded1d681c9
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-28 21:27:14 -0300

    :cyclone: Refactoring Features

---
+++ src/logged_out/Home/Features/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+import colors from '../../../styles/colors';
+
+export const Container = styled.section`
+  justify-content: center;
+
+  h1 {
+    color: ${colors.main};
+    font-size: 4rem;
+    text-align: center;
+    cursor: default;    
+    margin: 2rem 0;
+
+    ::-moz-selection {
+      color: ${colors.main};
+      background: transparent;
+    }
+  
+    ::selection {
+      color: ${colors.main};
+      background: transparent;
+    }
+  }
+
+  .row {
+    display: flex;
+    padding: 0 5rem 6rem 5rem;
+    background-color: ${colors.white};
+  }
+`;
