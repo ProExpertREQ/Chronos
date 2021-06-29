4252f4b803b3ac7623cf05f42b7b9861051a107d
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-28 21:25:57 -0300

    :cyclone: Refactoring Features array

---
+++ src/logged_out/Home/Features/featuresArray.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 0,
+    description: 'Você encontrará onde o professor ministrará as aulas, onde disponibilizará os materiais, quais os canais de comunicação com a turma',
+  },
+  {
+    id: 1,
+    description: 'Ajudaremos você a organizar o seu semestre, disponibilizando todas as informações das matérias que você está cursando',
+  },
+  {
+    id: 2,
+    description: 'Você também poderá marcar as suas faltas e iremos te avisar quando não poder faltar mais, sem  correr risco de perder todo o semestre',
+  },
+];
