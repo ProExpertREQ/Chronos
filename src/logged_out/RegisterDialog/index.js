13f6ebcc1281a9efd35de6efd7262b7ecf61cf3b
Douglas-Castro <farias.douglascastro@gmail.com>
2021-06-29 21:00:05 -0300

    :bug: Fix input autoComplete

--- src/logged_out/RegisterDialog/index.js
+++ src/logged_out/RegisterDialog/index.js
@@ -7,21 +7,49 @@ const RegisterDialog = ({onClose = () => {}}) => {
   return (
     <Modal title="Registrar" onClose={onClose}>
       <label htmlFor="name">Nome</label>
-      <input type="text" name="name" id="name" placeholder="Nise da Silveira" />
+      <input 
+        type="text" 
+        name="name" 
+        id="name" 
+        placeholder="Nise da Silveira" 
+        autoComplete="off" 
+      />
 
       <label htmlFor="email">E-mail</label>
-      <input type="text" name="email" id="email" placeholder="exemplo@exemplo.com" />
+      <input 
+        type="text" 
+        name="email" 
+        id="email" 
+        placeholder="exemplo@exemplo.com" 
+        autoComplete="off" 
+      />
 
       <label htmlFor="registration-number">Matrícula</label>
-      <input type="text" name="registration-number" id="registration-number" placeholder="21/0419620" />
+      <input 
+        type="text" 
+        name="registration-number" 
+        id="registration-number" 
+        placeholder="21/0419620" 
+        autoComplete="off" 
+      />
 
       <label htmlFor="password">Senha</label>
-      <input type="password" name="password" id="password" placeholder="Crie uma senha" />
+      <input 
+        type="password" 
+        name="password" 
+        id="password" 
+        placeholder="Crie uma senha" 
+      />
 
       <label htmlFor="password-confirm">Confirmar Senha</label>
-      <input type="password" name="password-confirm" id="password-confirm" placeholder="Confirme a senha" />
+      <input 
+        type="password" 
+        name="password-confirm" 
+        id="password-confirm" 
+        placeholder="Confirme a senha"
+      />
 
-      <label id="button-label">Button</label>
+      <label id="button-label">|</label>
       <Button id="register">Registrar-se</Button>
     </Modal>
   );
