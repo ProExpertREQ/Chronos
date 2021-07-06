a4732122dfaff7c70a0fcbb39f5729b74519a6e9
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-06 19:20:41 -0300

    :bulb: Update RegisterDialog

--- src/logged_out/RegisterDialog/index.js
+++ src/logged_out/RegisterDialog/index.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 
 import Modal from '../Modal';
 import TextInput from '../components/TextInput';
 import Button from './styles';
-
-const schema = Yup.object().shape({
-  name: Yup.string()
-    .min(3, 'Seu nome precisa ter três letras no mínimo.')
-    .required('Qual é o seu nome?'),
-  email: Yup.string()
-    .email('Esse e-mail não é válido =(')
-    .required('Você usará esse e-mail para se conectar.'),
-  registration_number: Yup.string()
-    .min(9, 'Sua matrícula deve ter nove dígitos.')
-    .max(9, 'Sua matrícula deve ter nove dígitos.')
-    .required('Insira sua matrícula.'),
-  password: Yup.string()
-    .min(5, 'Sua senha deve ter pelo menos cinco caracteres.')
-    .max(50, 'Sua senha deve ter no máximo 50 caracteres.')
-    .required('Insira uma combinação de pelo menos cinco letras, sinais de pontuação ou símbolos(como ! e &).'),
-  passwordConfirm: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'As senhas devem ser iguais.')
-    .required('Você precisa confirmar sua senha.'),
-});
+import validationSchema from './validationSchema';
 
 const RegisterDialog = ({ onClose }) => (
   <Modal title="Registrar" id="modal" onClose={onClose}>
@@ -37,7 +17,7 @@ const RegisterDialog = ({ onClose }) => (
         password: '',
         passwordConfirm: '',
       }}
-      validationSchema={schema}
+      validationSchema={validationSchema}
       onSubmit={(values, { setSubmitting }) => {
         setSubmitting(false);
       }}
