404bbf154625b76a41026c3f4e60997776d9a13e
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-07 17:01:24 -0300

    :bulb: Add Load component

--- src/logged_out/Main.js
+++ src/logged_out/Main.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { Context } from '../Context/AuthContext';
+
+import Load from '../shared/Load';
 import NavBar from './NavBar';
 import Home from './Home';
 import Footer from './Footer';
 
 export default function Main() {
+  const { loading } = useContext(Context);
+
   return (
     <>
+      {loading ? <Load /> : null}
       <NavBar />
       <Home />
       <Footer />
