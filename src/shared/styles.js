770f4b9314afd716fab65b883af7cf6fcb7eac97
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-07 17:02:56 -0300

    :bulb: Add Load styles

---
+++ src/shared/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #F2F9FA;
+  height: 100vh;
+  width: 100v;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Container;
