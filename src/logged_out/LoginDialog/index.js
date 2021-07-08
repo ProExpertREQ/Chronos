8383b0c0a046926e0dbe6e1723653543141becfe
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-08 16:06:33 -0300

    :arrow_up: Update - include login error

--- src/logged_out/LoginDialog/index.js
+++ src/logged_out/LoginDialog/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Formik, Form } from 'formik';
 
@@ -7,40 +7,56 @@ import Modal from '../Modal';
 import Button from './styles';
 import validationSchema from './validationSchema';
 
-const LoginDialog = ({ onClose, onSubmit }) => (
-  <Modal title="Entrar" id="modal" onClose={onClose}>
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        setSubmitting(false);
-        onSubmit(values);
-      }}
-    >
-      <Form>
-        <TextInput
-          label="E-mail"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Digite seu e-mail"
-          autoComplete="off"
-        />
-        <TextInput
-          label="Senha"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Sua senha"
-        />
-        <Button type="submit">Entrar</Button>
-      </Form>
-    </Formik>
-  </Modal>
-);
+import { Context } from '../../Context/AuthContext';
+
+const LoginDialog = ({ onClose, onSubmit }) => {
+  const { error } = useContext(Context);
+
+  return (
+    <Modal title="Entrar" id="modal" onClose={onClose}>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(false);
+          onSubmit(values);
+        }}
+      >
+        <Form>
+          <TextInput
+            label="E-mail"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Digite seu e-mail"
+            autoComplete="off"
+          />
+          <TextInput
+            label="Senha"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Sua senha"
+          />
+          {error
+            ? (
+              <div
+                style={{ color: '#FE5244', fontWeight: 400, fontSize: '1rem' }}
+              >
+                {error}
+
+              </div>
+            )
+            : null}
+          <Button type="submit">Entrar</Button>
+        </Form>
+      </Formik>
+    </Modal>
+  );
+};
 
 LoginDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
