6e61153efc5d7d17ee9d7e30ed5ab299367a15eb
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-12 17:56:58 -0300

    :fire: Remove button

--- src/logged_out/Home/Header/index.js
+++ src/logged_out/Home/Header/index.js
@@ -11,9 +11,6 @@ export default function Home() {
           <p className="item-1">Todas as</p>
           <p className="item-2">matérias</p>
           <p className="item-3">em um só lugar</p>
-          <a href="#features">
-            <button type="button">Saiba Mais</button>
-          </a>
         </div>
         <img src={Illustration} alt="DrawKit" />
       </Section>
