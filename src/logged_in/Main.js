88f20476e4ea8204ccc3d85fc17f29991e325ce6
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-16 20:26:48 -0300

    :arrow_up: Update Main logged-in

--- src/logged_in/Main.js
+++ src/logged_in/Main.js
@@ -1,45 +1,45 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import api from '../services/api';
-import { Context } from '../Context/AuthContext';
-
-import Load from '../shared/Load';
+import Body from './styles';
 import SideBar from './components/SideBar';
 
-export default function Main() {
-  const [users, setUsers] = useState([]);
-  const { loading, handleLogout, authenticated } = useContext(Context);
+const MyClasses = () => <h1>Minhas Turmas</h1>;
+const Departments = () => <h1>Departamentos</h1>;
+const Courses = () => <h1>Cursos</h1>;
+const Subjects = () => <h1>Matérias</h1>;
+const Classes = () => <h1>Turmas</h1>;
 
-  useEffect(() => {
-    (async () => {
-      if (authenticated) {
-        const { data } = await api.get('users/get-all');
-        setUsers(data);
-      }
-    })();
-  }, [authenticated]);
+export default function Main() {
+  const [sidebarHidden, setSidebarHidden] = useState(false);
 
   return (
-    <>
-      {loading ? <Load /> : null}
-      <SideBar />
-      <h1>Estou logado</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.registration_number}>
-            {user.name}
-            |
-            {user.registration_number}
-            |
-            {user.department}
-            |
-            {user.course}
-            |
-            {user.email}
-          </li>
-        ))}
-        <button type="button" onClick={handleLogout}>SAIR</button>
-      </ul>
-    </>
+    <Body>
+      <BrowserRouter>
+        <SideBar onColapse={(inactive) => {
+          setSidebarHidden(inactive);
+        }}
+        />
+        <div className={sidebarHidden ? 'sidebar-hidden' : 'container'}>
+          <Switch>
+            <Route exact path="/minhas-turmas">
+              <MyClasses />
+            </Route>
+            <Route exact path="/departamentos">
+              <Departments />
+            </Route>
+            <Route exact path="/cursos">
+              <Courses />
+            </Route>
+            <Route exact path="/materias">
+              <Subjects />
+            </Route>
+            <Route exact path="/turmas">
+              <Classes />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Body>
   );
 }
