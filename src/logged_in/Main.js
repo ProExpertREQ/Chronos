3486a4b1b3a5d60b49948d59cd866ba9855823df
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-19 21:34:31 -0300

    :bug: Fix Logged_in layout and sidebar routes

--- src/logged_in/Main.js
+++ src/logged_in/Main.js
@@ -1,45 +1,58 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 import Body from './styles';
-import SideBar from './components/SideBar';
 
-const MyClasses = () => <h1>Minhas Turmas</h1>;
-const Departments = () => <h1>Departamentos</h1>;
-const Courses = () => <h1>Cursos</h1>;
-const Subjects = () => <h1>Matérias</h1>;
-const Classes = () => <h1>Turmas</h1>;
+import SideBar from './components/SideBar';
+import MyClasses from './components/pages/MyClasses';
+import Departments from './components/pages/Departments';
+import Courses from './components/pages/Courses';
+import Subjects from './components/pages/Subjects';
+import Classes from './components/pages/Classes';
+
+import { SidebarContext } from '../Context/SidebarContext';
 
 export default function Main() {
-  const [sidebarHidden, setSidebarHidden] = useState(false);
+  const { hiddenSidebar } = useContext(SidebarContext);
 
   return (
     <Body>
-      <BrowserRouter>
-        <SideBar onColapse={(inactive) => {
-          setSidebarHidden(inactive);
-        }}
-        />
-        <div className={sidebarHidden ? 'sidebar-hidden' : 'container'}>
+      <Router>
+        <SideBar />
+        <div className={hiddenSidebar ? 'sidebar-hidden' : 'container'}>
           <Switch>
-            <Route exact path="/minhas-turmas">
-              <MyClasses />
-            </Route>
-            <Route exact path="/departamentos">
-              <Departments />
-            </Route>
-            <Route exact path="/cursos">
-              <Courses />
-            </Route>
-            <Route exact path="/materias">
-              <Subjects />
-            </Route>
-            <Route exact path="/turmas">
-              <Classes />
-            </Route>
+            <Route
+              exact
+              path="/minhas-turmas"
+              component={MyClasses}
+            />
+            <Route
+              exact
+              path="/departamentos"
+              component={Departments}
+            />
+            <Route
+              exact
+              path="/cursos"
+              component={Courses}
+            />
+            <Route
+              exact
+              path="/materias"
+              component={Subjects}
+            />
+            <Route
+              exact
+              path="/turmas"
+              component={Classes}
+            />
           </Switch>
         </div>
-      </BrowserRouter>
+      </Router>
     </Body>
   );
 }
