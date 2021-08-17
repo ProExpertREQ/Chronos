07ad58d9f6272ad96b4fb80b1565127aae17628c
Douglas-Castro <farias.douglascastro@gmail.com>
2021-08-16 23:05:39 -0300

    :arrow_up: Update global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,10 +3,14 @@ import { createGlobalStyle } from 'styled-components';
 import colors from './colors';
 
 export default createGlobalStyle`
+  * {
+    margin: 0;
+    color: ${colors.dark};
+  }
+
   body {
     background-color: ${colors.white};
     font-family: 'Baloo Tamma 2', cursive;
     font-weight: 900;
-    margin: 0;
   }
 `;
